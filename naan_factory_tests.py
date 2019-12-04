768afa34598454cc4245efb1826049182ef9c9ad
Sam Watson <SWatson@spartaglobal.com>
2019-12-04 15:29:41 +0000

    Testing functions

---
+++ naan_factory_tests.py
@@ -0,0 +1,18 @@
+from naan_factory_functions import *
+# Tests go here for separation of concerns
+
+print("testing make_dough with 'water' and 'flour'. Expected --> 'dough'")
+# print statements used for boolean test (True/False)
+print(make_dough('water', 'flour') == 'dough')
+print('when testing make_dough we got:', make_dough('water', 'flour'))
+
+print("testing bake_dough with 'dough'. Expected --> 'naan'")
+print(bake_dough('dough') == 'naan')
+print('when testing bake_dough we got:', bake_dough('dough'))
+
+print(make_dough1('water', 'flour') == 'dough')
+print(make_dough('water', 'water') == 'dough')
+print(make_dough('flour', 'flour') == 'dough')
+print(make_dough1('water', 'cement') == 'not dough')
+print(bake_dough('chicken') == 'not naan')
+print(bake_dough('DOUGH') == 'not naan')
